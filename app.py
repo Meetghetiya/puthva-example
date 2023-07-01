301d8b84cd1af1dc242df9b75879c55c59047b3b
Meet Ghetiya <89141817+Meetghetiya@users.noreply.github.com>
2023-07-01 18:02:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,30 +48,33 @@
 
 
 from flask import Flask, request
-import requests
+import telegram
 
 app = Flask(__name__)
 
-# Replace 'YOUR_BOT_TOKEN' with your actual Telegram bot token
-BOT_TOKEN = '6207495158:AAFaw81_BaW2bVMae73wuZcjsrGqVO--0ZE'
-TELEGRAM_API_BASE_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/'
+# Replace 'YOUR_TELEGRAM_TOKEN' with your actual Telegram bot token
+bot_token = '6207495158:AAFaw81_BaW2bVMae73wuZcjsrGqVO--0ZE'
+bot = telegram.Bot(token=bot_token)
 
-@app.route('/')
-def index():
-    return "Telegram bot is up and running!"
+@app.route('/', methods=['POST'])
+def webhook():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    handle_update(update)
+    return 'ok'
 
-@app.route(f'/{BOT_TOKEN}', methods=['POST'])
-def handle_update():
-    data = request.get_json()
-    if 'message' in data:
-        chat_id = data['message']['chat']['id']
-        message_text = data['message']['text']
-        send_message(chat_id, f'You said: {message_text}')
-    return 'OK'
+def handle_update(update):
+    # Check if the update contains a message
+    if update.message:
+        # Get the text of the incoming message
+        text = update.message.text
 
-def send_message(chat_id, text):
-    url = f'{TELEGRAM_API_BASE_URL}sendMessage'
-    data = {'chat_id': chat_id, 'text': text}
-    requests.post(url, json=data)
-
+        # Check if the message is a "/start" command
+        if text == '/start':
+            # Respond with a welcome message
+            welcome_message = "Welcome to your Telegram bot! Type any message, and I'll respond!"
+            bot.send_message(chat_id=update.message.chat_id, text=welcome_message)
+        else:
+            # Respond to other messages (you can customize this part based on your bot's functionality)
+            response_message = f"You said: {text}"
+            bot.send_message(chat_id=update.message.chat_id, text=response_message)
 
